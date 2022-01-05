a25d6c4005fde78eeb18cc239fa5fadd2494f8d4
Thibault Lanssade <tibolan@gmail.com>
2022-01-06 00:49:03 +0100

    configure static

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,11 @@
 <template>
-  <section class="home">
-    <user name="zadig"></user>
-    <user name="swann"></user>
-  </section>
+  <main class="home">
+
+  </main>
 </template>
 
 <script>
-import User from "../components/User";
-
 export default {
   name: 'Home',
-  components: {
-    User
-  }
 }
 </script>
